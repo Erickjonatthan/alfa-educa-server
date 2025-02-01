f3bd36b32f6eca7d6187954b70376fa9b82f2af2
Erick Jonathan <65685466+Erickjonatthan@users.noreply.github.com>
2025-02-01 14:46:32 -0300

    ajusta tabela usuarios para definir valores padrão para nivel e pontos

--- src/main/resources/db/migration/V8__alter-table-usuarios-fix-not-null.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE usuarios
-ALTER COLUMN nivel DROP NOT NULL,
-ALTER COLUMN pontos DROP NOT NULL;
-
-UPDATE usuarios SET nivel = 0, pontos = 0 WHERE nivel IS NULL OR pontos IS NULL;
-
-ALTER TABLE usuarios
-ALTER COLUMN nivel SET DEFAULT 0,
-ALTER COLUMN pontos SET DEFAULT 0;
